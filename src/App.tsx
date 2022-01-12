d9a7b6a5190b076ef9c1ab8185f5fc9201d7ab20
Wrufesh Dev <wrufesh@gmail.com>
2022-01-12 17:01:39 +0545

    image captured from camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+
+  let videoRef = useRef<HTMLVideoElement>(null);
+  let canvasRef = useRef<HTMLCanvasElement>(null);
+
+  let detect = () => {
+
+    let canvasElement = canvasRef.current as HTMLCanvasElement
+
+    canvasElement.getContext('2d')?.drawImage(videoRef.current as HTMLVideoElement, 0, 0, canvasElement.width, canvasElement.height)
+    
+
+   	let image_data_url = canvasElement.toDataURL('image/jpeg');
+
+   	console.log(image_data_url);
+  }
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
+      (videoRef.current as HTMLVideoElement).srcObject = stream
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video ref={videoRef} muted autoPlay={true}></video>
+      <button onClick={detect}>Scan</button>
+      <canvas ref={canvasRef} width="320" height="240"></canvas>
     </div>
   );
 }
