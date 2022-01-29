bd6cfec7785aec8f9f35cff1efdb333ae39ab570
Wrufesh Dev <wrufesh@gmail.com>
2022-01-29 10:25:12 +0545

    continious qr code detection applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
 
 function App() {
 
   let videoRef = useRef<HTMLVideoElement>(null);
   let canvasRef = useRef<HTMLCanvasElement>(null);
 
-  let detect = () => {
+  let pairWithServer = () => {
 
-    let canvasElement = canvasRef.current as HTMLCanvasElement
+  }
 
-    canvasElement.getContext('2d')?.drawImage(videoRef.current as HTMLVideoElement, 0, 0, canvasElement.width, canvasElement.height)
-    
-
-   	let image_data_url = canvasElement.toDataURL('image/jpeg');
-
-   	console.log(image_data_url);
-
-     
-     
-     {/*
-     // @ts-ignore */}
-    let barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13', 'qr_code']});
-    
+  let detect = async () => {
+    // let canvasElement = canvasRef.current as HTMLCanvasElement
+    // canvasElement.getContext('2d')?.drawImage(videoRef.current as HTMLVideoElement, 0, 0, canvasElement.width, canvasElement.height)
+    // let image_data_url = canvasElement.toDataURL('image/jpeg');
     {/*
      // @ts-ignore */}
-    barcodeDetector.detect(videoRef.current).then(detected => {
+    let barcodeDetector = new BarcodeDetector({ formats: ['code_39', 'codabar', 'ean_13', 'qr_code'] });
+
+    while (true) {
+      {/*
+       // @ts-ignore */}
+      let detected = await barcodeDetector.detect(videoRef.current)
       console.log(detected)
-    })
-
-    console.log('hey')
-
-
+      if (detected.length){
+        break
+      }
+    }
   }
 
   useEffect(() => {
@@ -40,12 +36,16 @@ function App() {
   })
 
   return (
-    <div>
-      <video ref={videoRef} muted autoPlay={true}></video>
-      <button onClick={detect}>Scan</button>
-      <canvas ref={canvasRef} width="320" height="240"></canvas>
+    <div className="relative h-screen w-screen bg-gray-600">
+      <div className="absolute inset-0 h-screen bg-zinc-700 bg-opacity-90 z-10">
+        <div className="flex justify-center items-center h-screen">
+          <button className="p-5 text-lg text-white bg-blue-700" onClick={detect}>Pair with server</button>
+        </div>
+      </div>
+      <video className="h-screen w-screen" ref={videoRef} muted autoPlay={true}></video>
+      {/* <button onClick={detect}>Scan</button>
+      <canvas ref={canvasRef} width="320" height="240"></canvas> */}
     </div>
   );
 }
-
 export default App;
